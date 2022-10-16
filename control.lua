030f921bf5233c709d6484e9263908937d38a1c1
Serhii Semenov <nkptpro@gmail.com>
2022-10-16 17:26:26 +0300

    Add multiplayer support

--- control.lua
+++ control.lua
@@ -1,62 +1,75 @@
+local this = {
+    players = {}
+}
+
 script.on_event(defines.events.on_player_cancelled_crafting, function(event)
-    if global.HandCraftPriority and (global.HandCraftPriority.reset_crafting or global.HandCraftPriority.no_action) then
+    local player = game.players[event.player_index]
+    local player_config = this.players[event.player_index]
+    if player_config and (player_config.reset_crafting or no_action) then
         return
     end
-    local player = game.players[event.player_index]
-    inventory_bonus = player.force.character_inventory_slots_bonus
-    global.HandCraftPriority = {}
-    global.HandCraftPriority.cancelled_crafting = true
-    global.HandCraftPriority.inventory_bonus = inventory_bonus
-    player.force.character_inventory_slots_bonus = inventory_bonus + 1000
-    global.HandCraftPriority.ingredients = {count = event.cancel_count, recipe = event.recipe}
+
+    if player.character.character_inventory_slots_bonus == 0 then
+        player.character.character_inventory_slots_bonus = 100
+    end
+    this.players[event.player_index] = {
+        ingredients = {count = event.cancel_count, recipe = event.recipe}
+    }
 end)
 
 script.on_event(defines.events.on_player_main_inventory_changed, function(event)
-    if global.HandCraftPriority and global.HandCraftPriority.cancelled_crafting then
-        local player = game.players[event.player_index]
-        if player.get_main_inventory().count_empty_stacks() < 1000 then
-            player.begin_crafting(global.HandCraftPriority.ingredients)
+    local player = game.players[event.player_index]
+    local player_config = this.players[event.player_index]
+    if not player_config or player_config.no_action then
+        return
+    end
+
+    if player_config.ingredients then
+        if player.get_main_inventory().count_empty_stacks() < 100 then
+            player.begin_crafting(player_config.ingredients)
             game.print({'inventory-full-message.main'})
-            player.force.character_inventory_slots_bonus = global.HandCraftPriority.inventory_bonus
-            global.HandCraftPriority = {}
+            player.character.character_inventory_slots_bonus = 0
+            this.players[event.player_index] = {}
             return
         end
-        if global.HandCraftPriority.reset_crafting then
-            if player.crafting_queue then
-                last_craft = player.crafting_queue[player.crafting_queue_size]
-                global.HandCraftPriority.ingredients = last_craft
-                player.cancel_crafting{index=last_craft.index, count=last_craft.count}
-                return
-            end
-        end
-        player.force.character_inventory_slots_bonus = global.HandCraftPriority.inventory_bonus
-        global.HandCraftPriority = {}
     end
+
+    if player_config.reset_crafting and player.crafting_queue then
+        local last_craft = player.crafting_queue[player.crafting_queue_size]
+        player_config.ingredients = last_craft
+        player.cancel_crafting{index=last_craft.index, count=last_craft.count}
+        return
+    end
+
+    player.character.character_inventory_slots_bonus = 0
+    this.players[event.player_index] = {}
 end)
 
 script.on_event("reset-craft", function(event)
     local player = game.players[event.player_index]
     if player.crafting_queue_size > 0 then
-        inventory_bonus = player.force.character_inventory_slots_bonus
-        last_craft = player.crafting_queue[player.crafting_queue_size]
-        global.HandCraftPriority = {}
-        global.HandCraftPriority.cancelled_crafting = true
-        global.HandCraftPriority.reset_crafting = true
-        global.HandCraftPriority.inventory_bonus = inventory_bonus
-        global.HandCraftPriority.ingredients = last_craft
-        player.force.character_inventory_slots_bonus = inventory_bonus + 1000
+        if player.character.character_inventory_slots_bonus == 0 then
+            player.character.character_inventory_slots_bonus = 100
+        end
+        local last_craft = player.crafting_queue[player.crafting_queue_size]
+
         player.cancel_crafting{index=last_craft.index, count=last_craft.count}
+        this.players[event.player_index] = {
+            reset_crafting = true,
+            ingredients = last_craft
+        }
     end
 end)
 
 script.on_event("promote-craft", function(event)
     local player = game.players[event.player_index]
     if player.crafting_queue_size > 1 then
-        inventory_bonus = player.force.character_inventory_slots_bonus
-        last_craft = player.crafting_queue[player.crafting_queue_size]
-        global.HandCraftPriority = {}
-        global.HandCraftPriority.no_action = true
-        player.force.character_inventory_slots_bonus = inventory_bonus + 1000
+        local inventory_bonus = player.character.character_inventory_slots_bonus
+        local last_craft = player.crafting_queue[player.crafting_queue_size]
+        player.character.character_inventory_slots_bonus = inventory_bonus + 1000
+        this.players[event.player_index] = {
+            no_action = true,
+        }
 
         crafts = {}
         index = 1
@@ -76,19 +89,20 @@ script.on_event("promote-craft", function(event)
             player.begin_crafting{count=next_craft.count, recipe=next_craft.recipe, silent=False}
         end
 
-        player.force.character_inventory_slots_bonus = inventory_bonus
-        global.HandCraftPriority = {}
+        player.character.character_inventory_slots_bonus = inventory_bonus
+        this.players[event.player_index] = {}
     end
 end)
 
 script.on_event("demote-craft", function(event)
     local player = game.players[event.player_index]
     if player.crafting_queue_size > 1 then
-        inventory_bonus = player.force.character_inventory_slots_bonus
-        last_craft = player.crafting_queue[player.crafting_queue_size]
-        global.HandCraftPriority = {}
-        global.HandCraftPriority.no_action = true
-        player.force.character_inventory_slots_bonus = inventory_bonus + 1000
+        local inventory_bonus = player.character.character_inventory_slots_bonus
+        local last_craft = player.crafting_queue[player.crafting_queue_size]
+        player.character.character_inventory_slots_bonus = inventory_bonus + 1000
+        this.players[event.player_index] = {
+            no_action = true,
+        }
 
         crafts = {}
         index = 1
@@ -107,7 +121,7 @@ script.on_event("demote-craft", function(event)
         next_craft = crafts[index]
         player.begin_crafting{count=next_craft.count, recipe=next_craft.recipe, silent=False}
 
-        player.force.character_inventory_slots_bonus = inventory_bonus
-        global.HandCraftPriority = {}
+        player.character.character_inventory_slots_bonus = inventory_bonus
+        this.players[event.player_index] = {}
     end
 end)
